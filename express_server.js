b9f1685908f169c93a953e5d6af518a45df8eee8
Anastassia <nastiatsv@gmail.com>
2021-11-08 20:05:40 -0800

    added a POST route to receive the form submission

--- express_server.js
+++ express_server.js
@@ -1,13 +1,21 @@
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");    //telling Express to use EJS as templating engine
 
+const bodyParser = require("body-parser");    //installed body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 
 app.get("/urls", (req, res) => {
@@ -19,6 +27,21 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  const result = Math.random().toString(36).substr(2,6);
+  return result;
+  //console.log(Math.random().toString(36))
+  //console.log(result);
+}
+
+generateRandomString();
+
 
 
 
